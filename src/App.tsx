f8e1873a6cd6186a20197d4b6a3bbea5fa81e44d
gvsuresh738 <gvsuresh738@gmail.com>
2024-12-24 00:02:40 +0530

    implement react webpack setup

--- src/App.tsx
+++ src/App.tsx
@@ -4,8 +4,8 @@ import "./styles/main.css";
 const App: React.FC = () => {
   return (
     <div className="p-6 bg-gray-800 text-white text-center">
-      <h1 className="text-4xl font-bold">App</h1>
-    </div>
+      <h1 className="text-4xl font-bold bg-red-400">App</h1>
+    </div> 
   );
 };
 
