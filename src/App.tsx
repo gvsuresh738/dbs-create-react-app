38b93bc746c19c7012fd0882ea8e1c88285df4a0
gvsuresh738 <gvsuresh738@gmail.com>
2024-12-24 00:32:51 +0530

    tailwind config setup

--- src/App.tsx
+++ src/App.tsx
@@ -4,7 +4,7 @@ import "./styles/main.css";
 const App: React.FC = () => {
   return (
     <div className="p-6 bg-gray-800 text-white text-center">
-      <h1 className="text-4xl font-bold bg-red-400">App</h1>
+      <h1 className="text-4xl font-bold bg-red-400">Happy hacking</h1>
     </div> 
   );
 };
